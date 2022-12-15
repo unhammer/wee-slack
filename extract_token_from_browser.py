e12aa33fceaeb4f00b966f71bc87c5a46b129f5b
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2022-12-15 22:26:38 +0100

    Extract token from all localstores

--- extract_token_from_browser.py
+++ extract_token_from_browser.py
@@ -27,7 +27,7 @@ else:
     sys.exit(1)
 
 try:
-    default_profile_path = next(firefox_path.glob("*.default-release"))
+    default_profile_path = next(firefox_path.glob("*.default"))
 except StopIteration:
     print("Couldn't find the default profile for Firefox", file=sys.stderr)
     sys.exit(1)
@@ -37,29 +37,33 @@ con = sqlite3.connect(f"file:{cookies_path}?immutable=1", uri=True)
 cookie_d_query = (
     "SELECT value FROM moz_cookies WHERE host = '.slack.com' AND name = 'd'"
 )
-cookie_d_value = con.execute(cookie_d_query).fetchone()[0]
+cookie_d_values = con.execute(cookie_d_query).fetchall()
 cookie_ds_query = (
     "SELECT value FROM moz_cookies WHERE host = '.slack.com' AND name = 'd-s'"
 )
-cookie_ds_values = con.execute(cookie_ds_query).fetchone()
+cookie_ds_values = con.execute(cookie_ds_query).fetchall()
 con.close()
+if len(cookie_d_values) != 1:
+    print("cookie_d_values did not have length 1, don't know how to deal: %s" % (cookie_d_values,), file=sys.stderr)
+    sys.exit(1)
 
-if cookie_ds_values:
-    cookie_value = f"d={cookie_d_value};d-s={cookie_ds_values[0]}"
+if len(cookie_ds_values) == 1:
+    cookie_value = f"d={cookie_d_values[0][0]};d-s={cookie_ds_values[0][0]}"
 else:
-    cookie_value = cookie_d_value
+    cookie_value = cookie_d_values[0][0]
 
 local_storage_path = default_profile_path.joinpath("webappsstore.sqlite")
 con = sqlite3.connect(f"file:{local_storage_path}?immutable=1", uri=True)
 local_storage_query = "SELECT value FROM webappsstore2 WHERE key = 'localConfig_v2'"
-local_config_str = con.execute(local_storage_query).fetchone()[0]
+local_config_strs = con.execute(local_storage_query).fetchall()
 con.close()
 
-local_config = json.loads(local_config_str)
-teams = [
-    team for team in local_config["teams"].values() if not team["id"].startswith("E")
-]
-register_commands = [
-    f"{team['name']}:\n/slack register {team['token']}:{cookie_value}" for team in teams
-]
-print("\n\n".join(register_commands))
+for local_config_str in local_config_strs:
+    local_config = json.loads(local_config_str[0])
+    teams = [
+        team for team in local_config["teams"].values() if not team["id"].startswith("E")
+    ]
+    register_commands = [
+        f"{team['name']}:\n/slack register {team['token']}:{cookie_value}" for team in teams
+    ]
+    print("\n\n".join(register_commands))
